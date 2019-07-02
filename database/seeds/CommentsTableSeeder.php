df94f91aa50112155b5ec9ffad4217891a694e3a
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-07-02 10:04:44 +0900

    seed

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -4,13 +4,21 @@ use Illuminate\Database\Seeder;
 
 class CommentsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('comments')->delete();
+
+    DB::table('comments')->insert([
+      'body' => '# タイトル'
+    ]);
+
+    DB::table('comments')->insert([
+      'body' => '## サブタイトル\n- 箇条書き1\n- 箇条書き項目2'
+    ]);
+  }
 }
